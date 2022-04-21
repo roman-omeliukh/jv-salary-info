a4f33d9c7e0b075ec27e36045b4f5fff9003cf10
Roman Omeliukh <romik3513@gmail.com>
2022-04-21 18:08:06 +0300

    salary-hometask

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,35 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        int salary;
+        StringBuilder salaryInfo = new StringBuilder("Report for period ");
+        salaryInfo.append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate datefrom = LocalDate.parse(dateFrom, formatter);
+        LocalDate dateto = LocalDate.parse(dateTo, formatter);
+        for (String name:names) {
+            salary = 0;
+            for (String fullData : data) {
+                String[] splitByWhiteSpace = fullData.split(" ");
+                LocalDate currentDate = LocalDate.parse(splitByWhiteSpace[0], formatter);
+                if (name.equals(splitByWhiteSpace[1])) {
+                    if ((currentDate.isAfter(datefrom)
+                            && currentDate.isBefore(dateto))
+                            || (currentDate.isEqual(datefrom))
+                            || (currentDate.isEqual(dateto))) {
+                        salary = salary + Integer.parseInt(splitByWhiteSpace[2])
+                                * Integer.parseInt(splitByWhiteSpace[3]);
+                    }
+                }
+            }
+            salaryInfo.append(System.lineSeparator()).append(name).append(" - ").append(salary);
+        }
+        return salaryInfo.toString();
     }
 }
