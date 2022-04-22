ec44213859971cf6d4290a9ca2be34aa32d4e4e6
Roman Omeliukh <romik3513@gmail.com>
2022-04-22 11:53:40 +0300

    fixed version salaryinfo

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,13 +4,15 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final String DATA = "dd.MM.yyyy";
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         int salary;
         StringBuilder salaryInfo = new StringBuilder("Report for period ");
         salaryInfo.append(dateFrom)
                 .append(" - ")
                 .append(dateTo);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATA);
         LocalDate datefrom = LocalDate.parse(dateFrom, formatter);
         LocalDate dateto = LocalDate.parse(dateTo, formatter);
         for (String name:names) {
@@ -18,14 +20,13 @@ public class SalaryInfo {
             for (String fullData : data) {
                 String[] splitByWhiteSpace = fullData.split(" ");
                 LocalDate currentDate = LocalDate.parse(splitByWhiteSpace[0], formatter);
-                if (name.equals(splitByWhiteSpace[1])) {
-                    if ((currentDate.isAfter(datefrom)
-                            && currentDate.isBefore(dateto))
-                            || (currentDate.isEqual(datefrom))
-                            || (currentDate.isEqual(dateto))) {
-                        salary = salary + Integer.parseInt(splitByWhiteSpace[2])
+                if ((name.equals(splitByWhiteSpace[1]))
+                        && ((currentDate.isAfter(datefrom)
+                        && currentDate.isBefore(dateto))
+                        || (currentDate.isEqual(datefrom))
+                        || (currentDate.isEqual(dateto)))) {
+                    salary = salary + Integer.parseInt(splitByWhiteSpace[2])
                                 * Integer.parseInt(splitByWhiteSpace[3]);
-                    }
                 }
             }
             salaryInfo.append(System.lineSeparator()).append(name).append(" - ").append(salary);
